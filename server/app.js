e1d308ec1a3bfde22adbd485967f02857c9c3319
Syam <syam@untitledproject.sg>
2021-03-13 19:26:59 +0800

    added docker files - server - client

--- server/app.js
+++ server/app.js
@@ -1,6 +1,4 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
@@ -22,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
 //Router configuration
---
+++ server/routes/offshore-data-router.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const offshoreDataService = require('../services/offshore-data-service');
+
+
+
+module.exports = router;
