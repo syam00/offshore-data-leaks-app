03b2ea828284cbdb50026d6587caa8348a8b7cc0
Syam <syam@untitledproject.sg>
2021-03-13 17:55:25 +0800

    re arranged the folders

---
+++ server/app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var helmet = require('helmet');
+var cors = require('cors');
+
+var app = express();
+
+app.use(
+    cors({
+      origin: [
+        'http://localhost:3000'
+      ],
+      credentials: true
+    })
+);
+  
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+
+const dbDriver = require('./db-driver');
+
+//Router configuration
+
+
+module.exports = app;
---
+++ server/db-driver.js
@@ -0,0 +1,28 @@
+// npm install --save neo4j-driver
+// node example.js
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('bolt://3.92.61.225:7687',
+                  neo4j.auth.basic('neo4j', 'tolerance-antenna-price'), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH (a:Officer {name:$name})-[r:OFFICER_OF|INTERMEDIARY_OF|REGISTERED_ADDRESS*..10]-(b) 
+  RETURN b.name as name LIMIT 20
+  `;
+
+const params = {"name": "Ross, Jr. - Wilbur Louis"};
+
+const session = driver.session({database:"neo4j"});
+
+session.run(query, params)
+  .then((result) => {
+    result.records.forEach((record) => {
+        console.log(record.get('name'));
+    });
+    session.close();
+    driver.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
